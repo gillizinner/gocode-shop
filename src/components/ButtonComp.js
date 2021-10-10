423d7e992d3a3dc19fdb4796b27d0824bb924973
gillizinner <79641089+gillizinner@users.noreply.github.com>
2021-10-10 22:17:09 +0300

    Toggle Button

--- src/components/ButtonComp.js
+++ src/components/ButtonComp.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-function ButtonComp({products}) {
+function ButtonComp() {
     const [text, setText]= useState("Click");
   return (
     <button onClick={()=>{if(text==="Click") setText(""); else setText("Click") }}>{text}</button> 
