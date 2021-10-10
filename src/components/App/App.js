12c048e668d482aede79bddba5bc149bc26c137f
gillizinner <79641089+gillizinner@users.noreply.github.com>
2021-10-10 17:16:31 +0300

    blah blah

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+//import logo from '.../logo.svg';
+import './App.css';
+import Header from "../Header/Header";
+import Products from "../Products/Products";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
