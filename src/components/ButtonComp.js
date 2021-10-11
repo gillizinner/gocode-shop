56819c37aa421f5ea5f22d44e9812490ec06752a
gillizinner <79641089+gillizinner@users.noreply.github.com>
2021-10-11 16:36:43 +0300

    Revert "Toggle Button"
    This reverts commit 423d7e992d3a3dc19fdb4796b27d0824bb924973.

--- src/components/ButtonComp.js
+++ src/components/ButtonComp.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-function ButtonComp() {
+function ButtonComp({products}) {
     const [text, setText]= useState("Click");
   return (
     <button onClick={()=>{if(text==="Click") setText(""); else setText("Click") }}>{text}</button> 
